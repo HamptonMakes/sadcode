a96bd3ad2aff322a0c0c804f4c3ad5cc637c433b
Kevin Kress <kkress@gmail.com>
2016-11-09 14:01:22 -0800

    hello in an empty world

---
+++ hello_world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char** arg) {
+   printf("Hello, World?\n\n");
+
+   printf("Hello, anyone?\n\n");
+
+
+   printf("please.\n\n\n");
+
+
+   printf("someone.\n\n\n");
+
+
+   printf("anyone.\n\n\n\n\n");
+
+
+
+   printf("hello?\n");
+}
